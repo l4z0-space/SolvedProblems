32785c9f205cd3016aff42334852cbfaab459711
Lazaron Shyta <33004139+l4z0-space@users.noreply.github.com>
2020-04-14 13:28:22 +0300

    Update Solution.cpp

--- Graphs/Dijkstra/Sending Mail/Solution.cpp
+++ Graphs/Dijkstra/Sending Mail/Solution.cpp
@@ -7,34 +7,32 @@ typedef vector<pii> vii;
 
 int INF = 1000000000;
 
-int goDijkstra(map<int,vii>AdjList,int start,int target){
+int goDijkstra( map<int,vii> AdjList,int start,int target){
 
     vi Distace(99999,INF);
-    priority_queue<pii,vii,greater<pii>>pq;
-    pq.push(pii(0,start)); // push the start element in Pqueue
-    Distace[start]=0;
+    priority_queue<pii,vii,greater<pii>>pq; // priority queue built by vector or pairs ( vii ) containing pairs of integers ( pii )
+    pq.push( pii(0,start) ); // push the start element in Pqueue
+    Distace[start] = 0;
 
 
-    while(!pq.empty()){
+    while( !pq.empty() ){
 
-        pii currEl= pq.top();pq.pop();
-
-        int currNode,currLatency;
+        pii currEl = pq.top();pq.pop();
+        int currNode, currLatency;
         currNode = currEl.second;   // assigns the current latency and node
         currLatency = currEl.first;
 
-        if(currLatency>Distace[currNode])continue;  // important check
+        if( currLatency > Distace[ currNode ] ) continue;  // important check
 
-        for(int i=0;i<(int)AdjList[currNode].size();i++){   // iterate through all neighbors
+        for( int i = 0; i < (int)AdjList[currNode].size() ; i++){   // iterate through all neighbors
 
             pii next = AdjList[currNode][i];
-            int nextNode=next.second;       // assigns next latency and node
-            int nextLatency=next.first;
-
+            int nextNode = next.second;       // assigns next latency and node
+            int nextLatency = next.first;
             int toUpdateLatency = Distace[currNode]+nextLatency;    // new latency
 
-            if(toUpdateLatency < Distace[nextNode]){
-                Distace[nextNode]= toUpdateLatency;     // relaxation operation
+            if( toUpdateLatency < Distace[nextNode] ){
+                Distace[nextNode] = toUpdateLatency;     // relaxation operation
                 pq.push(pii(nextLatency,nextNode));
             }
         }
@@ -45,17 +43,16 @@ int goDijkstra(map<int,vii>AdjList,int start,int target){
 
 int getInput(){
 
-    map<int,vii>AdjList;
-    int nServers,mCables,start,target;
+    map<int,vii> AdjList;
+    int nServers, mCables, start, target;
     cin >> nServers >> mCables >> start >> target;
-    int one,two,latency;
-    while(mCables--){
-        cin>>one>>two>>latency;
+    int one, two, latency;
+    while( mCables-- ){
+        cin >> one >> two >> latency;
         AdjList[one].push_back(pii(latency,two));
         AdjList[two].push_back(pii(latency,one));
     }
-
-    return goDijkstra(AdjList,start,target);
+    return goDijkstra( AdjList , start , target );
 }
 
 
@@ -63,17 +60,12 @@ int getInput(){
 
 int main(){
 
-    int cases;cin>>cases;
-    for(int i=1;i<=cases;i++){
+    int cases; cin >> cases;
+    for( int i = 1 ; i <= cases ; i++ ){
         int sol = getInput();
-
-        cout<<"Case #"<<i<<": ";
-
-        if(sol==INF)cout<<"unreachable";
-        else cout<<sol;
-
-        cout<<endl;
+        cout << "Case #" << i << ": ";
+        if ( sol == INF ) cout << "unreachable";
+        else cout << sol;
+        cout << "\n";
     }
-
-
 }
